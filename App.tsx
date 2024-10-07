6726ffc051266e0c87f67714a017e6fd9a7eebf8
KAMRONBEK <kamuranbek98@gmail.com>
2024-10-07 17:59:21 +0500

    created an expo web app, implemented functionality for generating random color

--- App.tsx
+++ App.tsx
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Clipboard from "expo-clipboard";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
+  const [color, setColor] = useState("#FFF");
+
+  const handleBackgroundPress = () => {
+    const generatedColor = `#${Math.random()
+      .toString(16)
+      .slice(-6)
+      .toUpperCase()}`;
+
+    setColor(generatedColor);
+  };
+
+  const handleCopyColor = async () => {
+    await Clipboard.setStringAsync(color).then(() => {
+      toast(`Successfully copied color: ${color}`);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={handleBackgroundPress}
+      style={[styles.container, { backgroundColor: color }]}
+    >
+      <Text style={styles.greetingText}>Hello there</Text>
+
+      <View style={styles.copyContainer}>
+        <TouchableOpacity style={styles.hexContainer} onPress={handleCopyColor}>
+          <Text style={styles.hexText}>{color}</Text>
+        </TouchableOpacity>
+        <Text style={styles.copyText}>Click to copy</Text>
+      </View>
+
+      <ToastContainer autoClose={1000} />
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  greetingText: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+
+  copyContainer: {
+    width: "10%",
+    marginTop: 15,
+  },
+
+  hexContainer: {
+    width: "100%",
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 8,
+    alignItems: "center",
+  },
+
+  hexText: {
+    fontSize: 20,
+    fontWeight: "600",
+    fontStyle: "italic",
+  },
+
+  copyText: {
+    marginTop: 8,
+    fontSize: 14,
+    textAlign: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,2 @@
+# test-task-expo-web
+# test-task-expo-web
